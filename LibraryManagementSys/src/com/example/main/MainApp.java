27e2e7c9f08928e5cfa34efd81877105f82176a0
M@xy <maxy@Maxys-Mac.local>
2024-08-20 20:03:05 +0700

    Ass1

--- LibraryManagementSys/src/com/example/main/MainApp.java
+++ LibraryManagementSys/src/com/example/main/MainApp.java
@@ -1,9 +1,52 @@
 package com.example.main;
 
+import com.example.model.Book;
+import com.example.model.Library;
+import com.example.model.Member;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Library library= new Library();
+		
+		Book b1 = new Book("To Kill a Mockingbird", "Harper Lee", "0061120081");
+		Book b2 = new Book("1984", "George Orwell", "0451524934");
+		Book b3 = new Book("Pride and Prejudice", "Jane Austen", "1503290565");
+		
+		library.addBook(b1);
+		library.addBook(b2);
+		library.addBook(b3);
+		
+		Member m1 = new Member("Min Maung Hein", 0001);
+		Member m2 = new Member("Kyaw Tay Za", 0002);
+		Member m3 = new Member("Kyaw Thiha", 0003);
+		
+		
+		library.addMember(m1);
+		library.addMember(m2);
+		library.addMember(m3);
+		
+		
+		library.borrowBook(m1, b3);
+		library.borrowBook(m2, b1);
+		library.borrowBook(m3, b2);
+		
+		library.displayMembers();
+		library.displayBooks();
+		
+		library.returnBook(m1, b3);
+		library.returnBook(m2, b1);
+		library.returnBook(m3, b2);
+		
+		library.displayMembers();
+		library.displayBooks();
+		
+		
+		
+		
+		
+		
 
 	}
 
--- LibraryManagementSys/src/com/example/model/Library.java
+++ LibraryManagementSys/src/com/example/model/Library.java
@@ -1,5 +1,58 @@
 package com.example.model;
 
+import java.util.ArrayList;
+
 public class Library {
+	private ArrayList<Book> books=new ArrayList<Book>();
+	private ArrayList<Member>members=new ArrayList<Member>();
+	
+	public Library() {
+		
+		
+	}
+	public void addBook(Book book) {
+		
+		books.add(book);
+	}
+	
+	public void addMember(Member member) {
+		
+		members.add(member);
+	}
+	public void borrowBook(Member member,Book book) {
+		
+		member.borrowBook(book);
+	}
+	public void returnBook(Member member,Book book) {
+		
+		member.returnBook(book);
+		
+	}
+	public Book findBook(String title) {
+		
+		for (Book book : books) {
+			
+			if(book.getTitle().equals(title)) {
+				
+				return book;
+			}
+		}
+		return null;
+		
+	}
+	public void displayBooks() {
+		
+		for (Book book:books) {
+			
+			System.out.print(book);
+		}
+	}
+	public void displayMembers() {
+		
+		for (Member member:members) {
+			
+			System.out.print(member);
+		}
+	}
 
 }
